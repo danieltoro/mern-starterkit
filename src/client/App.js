e2e21accf54224b6288e5346d53cc4cc6cb3f864
danieltoro <d.toro@outlook.com>
2018-07-02 23:51:18 -0400

    Uikit fixed

--- src/client/App.js
+++ src/client/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       <div className="uk-position-relative">
         <img src="../docs/images/light.jpg" alt="" />
           <div className="uk-position-top">
-            <nav className="uk-navbar-container uk-navbar-transparent" uk-navbar>
+            <nav className="uk-navbar-container uk-navbar-transparent" uk-navbar="true">
               <div className="uk-navbar-left">
                 <ul className="uk-navbar-nav">
                   <li className="uk-active"><a href="#">Active</a></li>
@@ -28,7 +28,7 @@ class App extends Component {
                         <li className="uk-nav-header">Header</li>
                         <li><a href="#">Item</a></li>
                         <li><a href="#">Item</a></li>
-                        <li className="uk-nav-divider"></li>
+                        <li className="uk-nav-divider" />
                         <li><a href="#">Item</a></li>
                       </ul>
                     </div>
